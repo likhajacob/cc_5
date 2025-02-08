a5e369c6874f70c42bb26c9988e714655f839eb9
Likha Jacob <likha.jacob03@gmail.com>
2025-02-08 18:51:47 -0500

    Task 1 - customer profile

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+// Task 1 - Object Properties
+const customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
