e326d7ff717e26b387417f99ba4bffcf36563af6
Likha Jacob <likha.jacob03@gmail.com>
2025-02-08 19:23:55 -0500

    Task 2 - order details

--- cc_5.js
+++ cc_5.js
@@ -8,3 +8,17 @@ const customer = {
 console.log(`Name: ${customer.name}`);
 console.log(`Age: ${customer.age}`);
 console.log(`Email: ${customer.email}`);
+
+// Task 2 - Object Methods
+const order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder: function() {
+        console.log(`Order ID: ${this.orderId}`);
+        console.log(`Total Amount: $${this.totalAmount}`);
+        console.log(`Status: ${this.status}`);
+    }
+};
+
+order.displayOrder();
